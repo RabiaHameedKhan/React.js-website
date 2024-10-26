5e812b0da544651e3fec6d3be5cdf86d2624ad46
RabiaHameedKhan <rabiahameedkhan27@gmail.com>
2024-10-27 03:09:04 +0500

    folder update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client'; 
 import './index.css';  
-import Navbar from './components/Navbar'; 
+import Navbar from './components/Navbar/Navbar'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import HeroSection from './components/HeroSection';
-import BottomSection from './components/Bottom';
-import Section1 from './components/section1';
-import ProjectSection from './components/Project';
-import Section2 from './components/Section2';
-import Teams from './components/Teams';
+import BottomSection from './components/BottomSection/Bottom';
+import Section1 from './components/Section1/section1';
+import ProjectSection from './components/Projects/Project';
+import Section2 from './components/Section2/Section2';
+import Teams from './components/Teams/Teams';
 
 
 function App() {
